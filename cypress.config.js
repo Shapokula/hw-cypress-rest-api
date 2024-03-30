8f6692c23974a76fea56f74a1a005bdf6bfeb0f4
Dora Charm <kovina.dora@gmail.com>
2024-03-30 15:05:09 +0500

    Connected to dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'dvnv6z',
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2",
     setupNodeEvents(on, config) {
